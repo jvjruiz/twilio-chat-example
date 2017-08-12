be52e83b1d1b9197c8bf4677e440a889ac76c6a4
JR <jovincent.ruiz@gmail.com>
2017-08-12 14:14:36 -0700

    first commit

---
+++ src/router.js
@@ -0,0 +1,49 @@
+const Router = require('express').Router;
+
+const {registerBind, sendNotification} = require('./notification_handler');
+const tokenGenerator = require('./token_generator');
+const config = require('./config');
+
+const router = new Router();
+
+// Convert keys to camelCase to conform with the twilio-node api definition contract
+const camelCase = require('camelcase');
+function camelCaseKeys(hashmap) {
+  var newhashmap = {};
+  Object.keys(hashmap).forEach(function(key) {
+    var newkey = camelCase(key);
+    newhashmap[newkey] = hashmap[key];
+  });
+  return newhashmap;
+};
+
+router.get('/token', (req, res) => {
+  res.send(tokenGenerator());
+});
+
+router.post('/token', (req, res) => {
+  const identity = req.body.identity;
+  res.send(tokenGenerator(identity));
+});
+
+router.post('/register', (req, res) => {
+  var content = camelCaseKeys(req.body);
+  registerBind(content).then((data) => {
+    res.status(data.status);
+    res.send(data.data);
+  });
+});
+
+router.post('/send-notification', (req, res) => {
+  var content = camelCaseKeys(req.body);
+  sendNotification(content).then((data) => {
+    res.status(data.status);
+    res.send(data.data);
+  });
+});
+
+router.get('/config', (req, res) => {
+  res.json(config);
+});
+
+module.exports = router;
---
+++ src/token_generator.js
@@ -0,0 +1,62 @@
+const Twilio = require('twilio');
+
+const config = require('./config');
+const nameGenerator = require('../name_generator');
+
+// Access Token used for Video, IP Messaging, and Sync
+const AccessToken = Twilio.jwt.AccessToken;
+const IpMessagingGrant = AccessToken.IpMessagingGrant;
+const VideoGrant = AccessToken.VideoGrant;
+const SyncGrant = AccessToken.SyncGrant;
+
+/**
+ * Generate an Access Token for an application user - it generates a random
+ * username for the client requesting a token or generates a token with an
+ * identity if one is provided.
+ *
+ * @return {Object}
+ *         {Object.identity} String random indentity
+ *         {Object.token} String token generated
+ */
+function tokenGenerator(identity = 0) {
+  // Create an access token which we will sign and return to the client
+  const token = new AccessToken(
+    config.TWILIO_ACCOUNT_SID,
+    config.TWILIO_API_KEY,
+    config.TWILIO_API_SECRET
+  );
+
+  // Assign the provided identity or generate a new one
+  token.identity = identity || nameGenerator();
+
+  // Grant the access token Twilio Video capabilities
+  const videoGrant = new VideoGrant();
+  token.addGrant(videoGrant);
+
+  if (config.TWILIO_CHAT_SERVICE_SID) {
+    // Create a "grant" which enables a client to use IPM as a given user,
+    // on a given device
+    const ipmGrant = new IpMessagingGrant({
+      serviceSid: config.TWILIO_CHAT_SERVICE_SID
+    });
+    token.addGrant(ipmGrant);
+  }
+
+  if (config.TWILIO_SYNC_SERVICE_SID) {
+    // Create a "grant" which enables a client to use Sync as a given user,
+    // on a given device
+    const syncGrant = new SyncGrant({
+      serviceSid: config.TWILIO_SYNC_SERVICE_SID
+    });
+    token.addGrant(syncGrant);
+  }
+
+  console.log(token)
+  // Serialize the token to a JWT string and include it in a JSON response
+  return {
+    identity: token.identity,
+    token: token.toJwt()
+  };
+}
+
+module.exports = tokenGenerator;
